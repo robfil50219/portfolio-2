b13e343e9e26369fb1022dbdafb999554e862efa
robfil50219 <robfil50219@stud.noroff.no>
2025-03-06 12:24:35 +0100

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,8 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container text-center">
-        <p>© {new Date().getFullYear()} Robert Filep</p>
-        <a href="/contact" className="contact-link">Contact Me</a>
-      </div>
+      <p className="footer-left">© {new Date().getFullYear()} Robert Filep</p>
+      <a href="/contact" className="footer-center">Contact Me</a>
     </footer>
   );
 }
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,4 +1,3 @@
-// src/pages/ContactPage.js
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 import { useNavigate } from 'react-router-dom';

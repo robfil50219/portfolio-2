f6fe5fa7bb4e8841698b73e9319eece0d6fccf5a
robfil50219 <robfil50219@stud.noroff.no>
2025-03-08 14:07:58 +0100

    header layout changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+// src/components/Header.js
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './Header.css';
@@ -14,28 +15,30 @@ function Header() {
 
   return (
     <header className="pixel-header container-fluid">
-      <div className="header-left">
-        <motion.img
-          src={process.env.PUBLIC_URL + '/images/profile-pic.JPG'}
-          alt="Profile"
-          className="header-profile-pic"
-          initial={{ rotate: 0 }}
-          animate={loading ? { rotate: 360 } : { rotate: 0 }}
-          transition={
-            loading
-              ? { repeat: Infinity, duration: 1, ease: 'linear' }
-              : { duration: 0.5 }
-          }
-          whileHover={
-            !loading
-              ? { rotate: 360, transition: { repeat: Infinity, duration: 1, ease: 'linear' } }
-              : {}
-          }
-        />
-        {loading && <LoadingText />}
-      </div>
-      <div className="header-text">
-        <h1>Welcome To My Portfolio</h1>
+      <div className="header-container">
+        <div className="header-left">
+          <motion.img
+            src={process.env.PUBLIC_URL + '/images/profile-pic.JPG'}
+            alt="Profile"
+            className="header-profile-pic"
+            initial={{ rotate: 0 }}
+            animate={loading ? { rotate: 360 } : { rotate: 0 }}
+            transition={
+              loading
+                ? { repeat: Infinity, duration: 1, ease: 'linear' }
+                : { duration: 0.5 }
+            }
+            whileHover={
+              !loading
+                ? { rotate: 360, transition: { repeat: Infinity, duration: 1, ease: 'linear' } }
+                : {}
+            }
+          />
+          {loading && <LoadingText />}
+        </div>
+        <div className="header-center">
+          <h1>Welcome To My Portfolio</h1>
+        </div>
       </div>
     </header>
   );
